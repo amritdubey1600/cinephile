60be00abed4c184f1a512820914f0b90d216a4c0
Amrit <amritdubey22003@gmail.com>
2025-08-21 19:48:56 +0530

    Delete Bookings Updated

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -1,14 +1,17 @@
 import { deleteBooking } from "@/lib/firebase/controllers/bookingControllers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, { params }: {params: { id: string }}) {
-    const bookingId = params.id;
+export async function DELETE(
+    req: NextRequest, 
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const { id: bookingId } = await params;
 
     try {
         await deleteBooking(bookingId);
-        return NextResponse.json({message: 'Booking deleted.'}, {status: 200});
+        return NextResponse.json({ message: 'Booking deleted.' }, { status: 200 });
     } catch (error) {
         console.log(error);
-        return NextResponse.json({error: 'Error in deleting booking.'}, {status: 400});
+        return NextResponse.json({ error: 'Error in deleting booking.' }, { status: 400 });
     }
 }
